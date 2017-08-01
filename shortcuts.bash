7ad6919332bc7d308f693713d4a895b0646cf9de
Kelli Parker <iamkelliparker@gmail.com>
2017-08-01 13:20:29 -0400

    include updates

---
+++ shortcuts.bash
@@ -0,0 +1,3 @@
+alias deployqa='cd ~/work/subway-stop-restaurant-lookup-table; scp -r . qa:/apps/eat-along-subway/'
+
+alias deployprod='cd ~/work/subway-stop-restaurant-lookup-table; scp -r . prod:/apps/eat-along-subway/'
