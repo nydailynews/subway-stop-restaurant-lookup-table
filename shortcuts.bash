dd324ca78b5866e51fe4efe29b4334e2263d44e3
Kelli Parker <iamkelliparker@gmail.com>
2018-02-21 16:31:25 -0500

    stash

--- shortcuts.bash
+++ shortcuts.bash
@@ -1,3 +1,3 @@
-alias deployqa='cd ~/work/eat-along-subway/www; scp -r . qa:/apps/eat-along-subway/'
+alias deployqa='cd ~/work/eat-along-subway/www; scp -r . qa:/apps/map/nyc-subway-restaurant-guide/'
 
 alias deployprod='cd ~/work/eat-along-subway/www; scp -r . prod:/apps/map/nyc-subway-restaurant-guide/'
